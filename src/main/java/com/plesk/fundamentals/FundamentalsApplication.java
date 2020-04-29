afbeafea067abbf6dd6fe8383581f78d6f437e62
peterlesko <peterlesko@yahoo.com>
2020-04-29 16:27:03 +0100

    first commit

---
+++ src/main/java/com/plesk/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.plesk.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsApplication.class, args);
+	}
+
+}
